0daea34372acc7e48d3a00951806217e0cfdca27
JoelmaBittencourt <83016532+JoelmaBittencourt@users.noreply.github.com>
2021-12-16 11:39:56 -0300

    when,if e else

---
+++ Classes/src/ControleFluxoWhen.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    var opcao = 15
+    when (opcao) {
+        1 -> println("opcao 1")
+        2 -> println("opcao 2")
+        3 -> println("opcao 3")
+        4 -> println("opcao 4")
+        5 -> println("opcao 5")
+        6 -> println("opcao 6")
+        7 -> println("opcao 7")
+        8 -> println("opcao 8")
+        9 -> println("opcao 9")
+        10 -> println("opcao 10")
+
+    else ->
+        println("escolha uma opcao")
+
+    }
+}
