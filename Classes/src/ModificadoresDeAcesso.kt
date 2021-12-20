40807759b70a32499946b927bff0418d682998c3
JoelmaBittencourt <83016532+JoelmaBittencourt@users.noreply.github.com>
2021-12-20 11:45:11 -0300

    metodos modificadores de acesso

---
+++ Classes/src/ModificadoresDeAcesso.kt
@@ -0,0 +1,102 @@
+/*
+private -> Visível apenas dentro da classe
+protected -> Funciona como o "private", mas é visível em subclasses
+public -> Visível em todo lugar, caso não seja definido é o padrão.
+internal -> Lembra o Public, mas sua visibilidade é
+restringida ao módulo, sendo então visível dentro de um
+mesmo módulo.
+*/
+//open class Animale {
+//
+//    //private , protected, public
+//    internal var nomes = "marley"
+//
+//    fun dormir(){
+//        println("Dormir")
+//    }
+//
+//}
+//class Caos : Animal() {
+//    fun exibeNome(){
+//        println( "Método exibe nome: $nomes" )
+//    }
+//}
+fun main(args: Array<String>) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    val caos = Caos()
+    caos.exibeNome()
+    //println( animal.nome )
+
+}
